3542b346b548648612a5fa4bb2cec6ce1f084134
faris <faris@mail.com>
2022-02-01 11:39:24 +0500

    Ah :poop: here we go again!

---
+++ src/pages/home.js
@@ -0,0 +1,9 @@
+import Container from "react-bootstrap/Container";
+
+export default function Home(props) {
+  return (
+    <Container>
+      <h1>Welcome to Murtaza Karlu Composing Center - Dera Ismail Khan</h1>
+    </Container>
+  );
+}
